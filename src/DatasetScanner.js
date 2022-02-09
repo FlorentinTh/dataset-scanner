09fae6a01a33852b835d463a31300aa02efdd307
FlorentinTh <florentin.thullier1@uqac.ca>
2022-02-09 10:56:22 -0500

    feat(dataset-scanner): add a condition related to the test command for the CI

--- src/DatasetScanner.js
+++ src/DatasetScanner.js
@@ -1,10 +1,20 @@
+import os from 'os';
+
 import QuestionsHelper from './helpers/QuestionHelper.js';
 import { Scanner } from './Scanner.js';
 
 class DatasetScanner {
   async run() {
-    const inputPathAnswer = await QuestionsHelper.askInputPath();
-    const scanner = new Scanner(inputPathAnswer.fs.path);
+    let inputPath;
+
+    if (process.env.NODE_ENV === 'test') {
+      inputPath = os.tmpdir();
+    } else {
+      const inputPathAnswer = await QuestionsHelper.askInputPath();
+      inputPath = inputPathAnswer.fs.path;
+    }
+
+    const scanner = new Scanner(inputPath);
     await scanner.scan();
   }
 }
